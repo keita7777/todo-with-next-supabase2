f8f366e1eeeeafd730885910c325f000f4a2a82c
github keita7777 <github mine0kei@gmail.com>
2024-08-11 07:11:50 +0900

    recoilの導入

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import SortTodo from "./components/SortTodo";
 import TodoList from "./components/TodoList";
+import { RecoilProvider } from "./recoil/RecoilProvider";
 
 export default function Home() {
   return (
-    <>
+    <RecoilProvider>
+      <SortTodo />
       <TodoList />
-    </>
+    </RecoilProvider>
   );
 }
---
+++ src/app/recoil/atom.ts
@@ -0,0 +1,6 @@
+import { atom } from "recoil";
+
+export const sortStates = atom({
+  key: "sortStates",
+  default: "all",
+});
